e680700df5338ab8696207ddb877ab91dd5e4aa9
aralyekta <aralyektayarimca@gmail.com>
2025-08-05 11:40:11 +0300

    Fix copy button size

--- src/widget.js
+++ src/widget.js
@@ -4784,7 +4784,7 @@ class ChatWidget {
     `;
 
     button.innerHTML = `
-    <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg width="16" height="16" viewBox="${type === 'copy' ? '0 0 16 16' : '0 0 24 24'}" fill="none" xmlns="http://www.w3.org/2000/svg">
       ${icon}
     </svg>
     ${text}
